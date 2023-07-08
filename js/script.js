ee0b019f805f8a7ccb58325623e1cae248f7f8a6
DarshanHeble <109078053+DarshanHeble@users.noreply.github.com>
2023-07-09 01:33:15 +0530

    successfull contact form

--- js/script.js
+++ js/script.js
@@ -86,31 +86,15 @@ wraper1.addEventListener("click", function () {
 
 // ---------------------------menue bar end----------------------------
 // ---------------------------form script start----------------------------
-let form = document.querySelector("form");
-// The document.querySelector() method returns the first element that matches a specified CSS selector.
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // The preventDefault() method stops the form from submitting and prevents the page from refreshing.
-  document.querySelector("#sub").value = "Submiting..";
-  // Change the value of the submit button to "Submitting..." while the form is being submitted.
-  let data = new FormData(form);
-  // The FormData() method creates a new FormData object that contains the values of all the form fields.
-  fetch('https://script.google.com/macros/s/AKfycbwbTF_cdJPwlVw9DcOancn7FUU83P2etZ1aMOAifZscdvay1Adx06vZuJZC_VQMDUg5/exec', {
-    method: "POST",
-    body: data
-  })
-    // The fetch() method is used to make a request to the server and retrieve data.
-    // This is an example API endpoint. Replace it with the actual URL for the API endpoint you want to use.
-    .then(res => res.text())
-    // The .then() method is used to handle the response from the server.
-    // The response is converted to text using the res.text() method.
-    .then(data => {
-      document.querySelector("#msg").innerHTML = data;
-      // The innerHTML property sets or returns the HTML content of an element.
-      // Here, we are setting the content of the <h1> element to the response from the server.
-      document.querySelector("#sub").value = "Submit"
-      // Change the value of the submit button back to "Submit" after the form has been submitted.
-    });
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyxKT3Z3siXDd_G89lY5Keq98n8Gh5sOdRGkqcsBNYpK8OMeqKKth_Uevg_ZKRHvDIMrg/exec'
+const form = document.forms['form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! your form is submitted successfully."))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
 })
 
 // ---------------------------form script end----------------------------
